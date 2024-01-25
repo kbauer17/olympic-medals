5287a4e62ed167cc76c2093d009fc0f851590d10
Kathy Bauer <kbauer17@my.wctc.edu>
2024-01-25 14:50:49 -0600

    [1.06] Add buttons - import bootstrap into App.js - create methods in Country.jsx - add buttons to Country.jsx

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -6,15 +6,29 @@ class Country extends Component {
         gold: 0,
      }
 
+    handleIncrement = () => {
+        this.setState({ gold: this.state.gold + 1 })
+    };
+
+    handleDecrement = () => {
+        if (this.state.gold <= 0){
+            this.setState({gold: 0})
+        } else{
+            this.setState({ gold: this.state.gold - 1 })
+        }
+    };
+
     render () {
         return ( 
             <div className="container-fluid">
-                <div className="name">
-                    {  this.state.name }
+                <div className="name">{  this.state.name }</div>
+                <div className="gold">Gold medals:  {  this.state.gold }
+                <div className="btn-group px-3" role="group" aria-label="Basic example">
+                        <button type="button" className="btn btn-primary btn-sm" onClick={this.handleIncrement}>+</button>
+                        <button type="button" className="btn btn-primary btn-sm" onClick={this.handleDecrement}>-</button>
+                    </div>
                 </div>
-                <div className="gold">
-                    Gold medals:  {  this.state.gold }
-                </div>
+                
                 
             </div>    
         )
