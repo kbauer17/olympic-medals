e33ca8f9c592c303ffc6878ca01aeb5fa394aed7
Kathy Bauer <kbauer17@my.wctc.edu>
2024-01-25 14:24:17 -0600

    [1.05] Create App.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import Country from './components/Country';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Country />
+    <App />
   </React.StrictMode>
 );
 
