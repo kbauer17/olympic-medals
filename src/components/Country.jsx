921e6637c94b1079a622bd402c157de76c2df9b0
Kathy Bauer <kbauer17@my.wctc.edu>
2024-01-25 13:54:27 -0600

    [1.02] add component -Country - update index.js to use Country component

---
+++ src/components/Country.jsx
@@ -0,0 +1,10 @@
+import React, { Component} from "react";
+
+class Country extends Component {
+    state = {  }
+
+    render () {
+        return ( <div>Hello Country!</div>)
+    }
+}
+export default Country
